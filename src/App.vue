f7e4ab771f8dbdddf5c97158d3b85e63cfbdcada
alphasqc <sqc19990726@163.com>
2023-08-15 16:45:12 +0800

    create the project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="common-layout">
+
+    <el-container height="100%">
+
+      <el-aside width="300px" style="background-color: aqua;">
+        Aside
+      </el-aside>
+
+      <el-container>
+
+        <el-header style="background-color: azure;">
+          Header
+        </el-header>
+
+        <el-main style="background-color: blanchedalmond;">
+          Main
+        </el-main>
+
+      </el-container>
+      
+    </el-container>
+
+  </div>
+
+</template>
+
+<script setup>
+</script>
